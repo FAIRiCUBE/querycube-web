e9c942e06a95f68e166be912dfd0825f541aa1fa
Bendik Svalastog <bsva@nilu.no>
2025-06-16 10:37:39 +0200

    add second map layer for visual effect.

--- client/src/components/Map.vue
+++ client/src/components/Map.vue
@@ -22,6 +22,10 @@
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M6 20q-.825 0-1.412-.587T4 18v-2q0-.425.288-.712T5 15t.713.288T6 16v2h12v-2q0-.425.288-.712T19 15t.713.288T20 16v2q0 .825-.587 1.413T18 20zm5-12.15L9.125 9.725q-.3.3-.712.288T7.7 9.7q-.275-.3-.288-.7t.288-.7l3.6-3.6q.15-.15.325-.212T12 4.425t.375.063t.325.212l3.6 3.6q.3.3.288.7t-.288.7q-.3.3-.712.313t-.713-.288L13 7.85V15q0 .425-.288.713T12 16t-.712-.288T11 15z" /></svg>
         </button>
       </div>
+      <div class="button-group">
+        <button @click="switchToDefaultMap">Default Map</button>
+        <button @click="switchToTopoMap">Topo Map</button>
+      </div>
     </div>
     <div id="map" class="fullscreen-map"></div>
   </div>
@@ -37,15 +41,16 @@ export default {
   emits: ["pointSelected"],
   data() {
     return {
-      savedPoints: []
+      savedPoints: [],
+      currentLayer: null // Track the current map layer
     };
   },
   mounted() {
     // Initialize the map
     this.map = L.map("map").setView([48.2052, 16.3599], 16); // Correct coordinates for the Natural History Museum in Vienna, Austria
 
-    // Add OpenStreetMap tiles
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    // Add default map tiles
+    this.currentLayer = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       maxZoom: 19,
       attribution: "© OpenStreetMap contributors"
     }).addTo(this.map);
@@ -87,6 +92,20 @@ export default {
     });
   },
   methods: {
+    switchToDefaultMap() {
+      if (this.currentLayer) this.map.removeLayer(this.currentLayer);
+      this.currentLayer = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 19,
+        attribution: "© OpenStreetMap contributors"
+      }).addTo(this.map);
+    },
+    switchToTopoMap() {
+      if (this.currentLayer) this.map.removeLayer(this.currentLayer);
+      this.currentLayer = L.tileLayer("https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png", {
+        maxZoom: 17,
+        attribution: "© OpenStreetMap contributors, © OpenTopoMap"
+      }).addTo(this.map);
+    },
     deletePoint(index) {
       this.savedPoints.splice(index, 1);
     },
